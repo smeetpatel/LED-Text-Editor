f7c83aaaae0af84c80b01cb50bcb55002b353e68
Smeet Patel <smeetp18@gmail.com>
2016-06-25 14:28:44 +0530

    Successfull CMDPARSE source file

--- cmdparser.h
+++ cmdparser.h
@@ -1,21 +1,26 @@
 #ifndef _cmdparser_h_included_
 #define _cmdparser_h_included_
-
+#include <list>
+#include <string>
+#include <iostream>
+class LED;
 class command{
 	bool valid;
 	char cmdsymbol,seprator;
 	char add1,add2;
-	list<char> parse;
+	std::list<char> parse;
 	//int i=0,sizemanage=0;
-	list<char>::iterator pitr;
+	std::list<char>::iterator pitr;
 	public:
+	friend class LED;
 	command();
-	void commandParser(string &,int,int);
-	void theta(string ,int,int);
-	void beta(string,int,int);
-	void alpha(string,int,int);
-	void myu(string,int,int);
+	void commandParser(std::string,int,int);
+	void theta(std::string ,int,int);
+	void beta(std::string,int,int);
+	void alpha(std::string,int,int);
+	void myu(std::string,int,int);
 	void print();
 };
+
 
 #endif
